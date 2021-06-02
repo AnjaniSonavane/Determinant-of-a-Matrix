4b242f0e07288d541bdc75a1ca399b2b59a6009b
AnjaniSonavane <83543597+AnjaniSonavane@users.noreply.github.com>
2021-06-02 15:24:09 +0530

    Add files via upload

---
+++ DeterminantOfMatrix.py
@@ -0,0 +1,17 @@
+# importing Numpy package
+import numpy as np
+
+# creating a 3X3 Numpy matrix
+n_array = np.array([[0, 2, 5],
+                    [2, 1, 1],
+                    [3, 1, 0]])
+
+# Displaying the Matrix
+print("Numpy Matrix is:")
+print(n_array)
+
+# calculating the determinant of matrix
+det = np.linalg.det(n_array)
+
+print("\nDeterminant of given 3X3 matrix:")
+print(float(det))
